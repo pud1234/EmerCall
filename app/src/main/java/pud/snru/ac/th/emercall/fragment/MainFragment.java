d65edd5eefe64e57f7507ac7d89fd2b312b0b00d
Admin <podziww26@gmail.com>
2018-02-20 11:34:04 +0700

    Create fragment Csuccess

---
+++ app/src/main/java/pud/snru/ac/th/emercall/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package pud.snru.ac.th.emercall.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import pud.snru.ac.th.emercall.R;
+
+/**
+ * Created by Admin on 20/2/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_police, container,false);
+        return mj;
+    }
+} //Main class
